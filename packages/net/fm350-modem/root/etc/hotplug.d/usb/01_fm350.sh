b84bfca7e366ff325c8b234888b94c790729f795
redbotu <darkmatch@163.com>
2025-05-15 14:13:24 +0800

    优化--需要openwrt自带驱动

--- packages/net/fm350-modem/root/etc/hotplug.d/usb/01_fm350.sh
+++ packages/net/fm350-modem/root/etc/hotplug.d/usb/01_fm350.sh
@@ -15,14 +15,14 @@ find_fm350_iface() {
 	        proto_set_available "$cfg" 1
 	fi
 }
-
-if [ "$PRODUCT" = 'e8d/7126/1' ] && [ "$ACTION" = add ]; then
-    echo "0e8d 7126 ff" > /sys/bus/usb-serial/drivers/option1/new_id
-fi
-
-if [ "$PRODUCT" = 'e8d/7127/1' ] && [ "$ACTION" = add ]; then
-    echo "0e8d 7127 ff" > /sys/bus/usb-serial/drivers/option1/new_id
-fi
+#驱动还是集成到openwrt里方便调试
+#if [ "$PRODUCT" = 'e8d/7126/1' ] && [ "$ACTION" = add ]; then
+#    echo "0e8d 7126 ff" > /sys/bus/usb-serial/drivers/option1/new_id
+#fi
+#
+#if [ "$PRODUCT" = 'e8d/7127/1' ] && [ "$ACTION" = add ]; then
+#    echo "0e8d 7127 ff" > /sys/bus/usb-serial/drivers/option1/new_id
+#fi
 
 config_load network
 config_foreach find_fm350_iface interface
